d75a2188bda84b76c6bdbe4c57deb58423d921f7
rahul9369 <rp25082003@gmail.com>
2024-10-09 18:55:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,57 @@ import Skill from "./Components/Skill";
 import Project from "./Components/Project";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        {/* Primary Meta Tags */}
+        <title>Rahul Prajapati - Portfolio</title>
+        <meta name="title" content="Rahul Prajapati - Portfolio" />
+        <meta
+          name="description"
+          content="Welcome to my portfolio! I'm a passionate web developer specializing in React.js, Tailwind CSS, and more. Check out my projects and resume."
+        />
+        <meta
+          name="keywords"
+          content="Rahul Prajapati, web developer, React.js developer, Tailwind CSS, JavaScript, portfolio, frontend developer, full stack developer, projects, resume"
+        />
+        <meta name="author" content="Rahul Prajapati" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://rahulprajapati25.netlify.app/"
+        />
+        <meta property="og:title" content="Rahul Prajapati - Portfolio" />
+        <meta
+          property="og:description"
+          content="Welcome to my portfolio! I'm a passionate web developer specializing in React.js, Tailwind CSS, and more. Check out my projects and resume."
+        />
+        <meta
+          property="og:image"
+          content="https://rahulprajapati25.netlify.app/Logo.jpg"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://rahulprajapati25.netlify.app/"
+        />
+        <meta property="twitter:title" content="Rahul Prajapati - Portfolio" />
+        <meta
+          property="twitter:description"
+          content="Welcome to my portfolio! I'm a passionate web developer specializing in React.js, Tailwind CSS, and more. Check out my projects and resume."
+        />
+        <meta
+          property="twitter:image"
+          content="https://rahulprajapati25.netlify.app/Logo.jpg"
+        />
+      </Helmet>
+
       <Header />
       <Hero />
       <About />
